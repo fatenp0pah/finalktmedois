c42c539843019145e31afd68ebeaa2470ad4aae4
fatenp0pah <ftnaffh@gmail.com>
2026-06-15 23:51:05 +0800

    Fix index.php - restore original paths for Railway

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -11,11 +10,15 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../laravel/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__ . '/../laravel/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$app->handleRequest(Request::capture());
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
